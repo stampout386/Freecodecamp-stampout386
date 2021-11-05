ef6f0bb6c93ba1d723416a397d07ea1e5792ff7b
Mikhail Shestak <stampout386@gmail.com>
2021-11-05 17:40:20 +0300

    pflfxb 6 - 7

---
+++ basicJs.js
@@ -0,0 +1,35 @@
+var a;
+a = 7;
+
+var a;
+a = 7;
+var b;
+b = a;
+
+var a = 9;
+
+var myFirstName = 'Mikhail';
+var myLastName = 'Shestak';
+
+// Only change code below this line
+var a;
+var b;
+var c;
+a = 5;
+b = 10;
+c = "I am a"
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
