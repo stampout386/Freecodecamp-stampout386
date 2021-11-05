a4f1726e81f17435307c12b24df63d55567d7e6b
Mikhail Shestak <stampout386@gmail.com>
2021-11-05 19:38:20 +0300

    20-26

--- basicJs.js
+++ basicJs.js
@@ -61,3 +61,44 @@ let myDecimal = 6.8;
 // Only change code below this line
 const product = 2.0 * 2.5;
 const quotient = 4.4 / 2.0;
+
+const remainder = 11%3;
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
