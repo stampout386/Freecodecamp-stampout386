530dedd5f45835e8b7a20596865eb5a63cca410b
Mikhail Shestak <stampout386@gmail.com>
2021-11-08 22:32:19 +0300

    regular expressions 1-5

--- basicJs.js
+++ basicJs.js
@@ -215,3 +215,15 @@ const myList = [
     ['coffee', 1],
     ['milk', 2],
 ];
+
+let petString = 'James has a pet cat.';
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+
+let myString = 'freeCodeCamp';
+let fccRegex = /freecodecamp/i; // Change this line
+let result = fccRegex.test(myString);
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
