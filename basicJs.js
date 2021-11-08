e36aa2a1617663f1e0611bc5b71812639e286c21
Mikhail Shestak <stampout386@gmail.com>
2021-11-08 16:21:12 +0300

    21-48

--- basicJs.js
+++ basicJs.js
@@ -17,12 +17,12 @@ var b;
 var c;
 a = 5;
 b = 10;
-c = "I am a"
+c = 'I am a';
 // Only change code above this line
 
 a = a + 1;
 b = b + 5;
-c = c + " String!";
+c = c + ' String!';
 
 // Variable declarations
 var studlyCapVar;
@@ -31,15 +31,15 @@ var titleCaseOver;
 
 // Variable assignments
 studlyCapVar = 10;
-properCamelCase = "A String";
+properCamelCase = 'A String';
 titleCaseOver = 9000;
 
-let catName = "Oliver";
-let catSound = "Meow!";
+let catName = 'Oliver';
+let catSound = 'Meow!';
 
-const FCC = "freeCodeCamp"; // Change this line
-let fact = "is cool!"; // Change this line
-fact = "is awesome!";
+const FCC = 'freeCodeCamp'; // Change this line
+let fact = 'is cool!'; // Change this line
+fact = 'is awesome!';
 console.log(FCC, fact); // Change this line
 
 const sum = 10 + 10;
@@ -62,7 +62,7 @@ let myDecimal = 6.8;
 const product = 2.0 * 2.5;
 const quotient = 4.4 / 2.0;
 
-const remainder = 11%3;
+const remainder = 11 % 3;
 
 let a = 3;
 let b = 17;
@@ -100,5 +100,118 @@ a /= 12;
 b /= 4;
 c /= 11;
 
-const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+const myStr = 'I am a "double quoted" string inside "double quotes".';
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+const myName = 'Mikhail';
+const myStr = 'My name is ' + myName + 'and I am well!';
+
+const someAdjective = 'great';
+let myStr = 'Learning to code is ';
+myStr += someAdjective;
+
+// Setup
+let lastNameLength = 0;
+const lastName = 'Lovelace';
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// Setup
+let firstLetterOfLastName = '';
+const lastName = 'Lovelace';
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
+
+// Setup
+let myStr = 'Jello World';
+
+// Only change code below this line
+myStr = 'Hello World'; // Change this line
+// Only change code above this line
+
+// Setup
+const lastName = 'Lovelace';
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// Setup
+const lastName = 'Lovelace';
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+// Setup
+const lastName = 'Lovelace';
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+const myNoun = 'dog';
+const myAdjective = 'big';
+const myVerb = 'ran';
+const myAdverb = 'quickly';
+
+// Only change code below this line
+const wordBlanks = myNoun + ' ' + myAdjective + ' ' + myVerb + ' ' + myAdverb; // Change this line
+// Only change code above this line
+const myArray = ['hello', 1];
+
+const myArray = [
+    ['Bulls', 23],
+    ['White Sox', 45],
+];
+
+const myArray = [50, 60, 70];
+
+let myData = myArray[0];
+
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
+
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+];
+
+const myData = myArray[2][1];
+
+// Setup
+const myArray = [
+    ['John', 23],
+    ['cat', 2],
+];
+
+// Only change code below this line
+myArray.push(['dog', 3]);
+
+const myArray = [
+    ['John', 23],
+    ['cat', 2],
+];
+
+// Only change code below this line
+const removedFromMyArray = myArray.pop();
+const myArray = [
+    ['John', 23],
+    ['dog', 3],
+];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(['Paul', 35]);
+
+const myList = [
+    ['bred', 2],
+    ['chokolate', 4],
+    ['tea', 1],
+    ['coffee', 1],
+    ['milk', 2],
+];
