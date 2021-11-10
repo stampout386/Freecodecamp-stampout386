fc071d9bc7903ea433beed3eb80deed5e11f4e9a
Mikhail Shestak <stampout386@gmail.com>
2021-11-10 15:08:07 +0300

    62-75

--- basicJs.js
+++ basicJs.js
@@ -335,3 +335,185 @@ function compareEquality(a, b) {
 }
 
 compareEquality(10, '10');
+
+function testNotEqual(val) {
+    if (val != 99) {
+        return 'Not Equal';
+    }
+    return 'Equal';
+}
+
+testNotEqual(10);
+
+function testStrictNotEqual(val) {
+    if (val !== 17) {
+        return 'Not Equal';
+    }
+    return 'Equal';
+}
+
+testStrictNotEqual(10);
+
+function testGreaterThan(val) {
+    if (val > 100) {
+        return 'Over 100';
+    }
+
+    if (val > 10) {
+        return 'Over 10';
+    }
+
+    return '10 or Under';
+}
+
+testGreaterThan(10);
+
+function testGreaterOrEqual(val) {
+    if (val >= 20) {
+        return '20 or Over';
+    }
+
+    if (val >= 10) {
+        return '10 or Over';
+    }
+
+    return 'Less than 10';
+}
+
+testGreaterOrEqual(10);
+
+function testLessThan(val) {
+    if (val < 25) {
+        return 'Under 25';
+    }
+
+    if (val < 55) {
+        return 'Under 55';
+    }
+
+    return '55 or Over';
+}
+
+testLessThan(10);
+
+function testLessOrEqual(val) {
+    if (val <= 12) {
+        return 'Smaller Than or Equal to 12';
+    }
+
+    if (val <= 24) {
+        return 'Smaller Than or Equal to 24';
+    }
+
+    return 'More Than 24';
+}
+
+testLessOrEqual(10);
+
+function testLogicalAnd(val) {
+    if (val >= 25 && val <= 50) {
+        return 'Yes';
+    }
+    return 'No';
+}
+
+testLogicalAnd(10);
+
+function testLogicalOr(val) {
+    // Only change code below this line
+
+    if (val < 10 || val > 20) {
+        return 'Outside';
+    }
+    return 'Inside';
+}
+
+testLogicalOr(15);
+
+function testElse(val) {
+    let result = '';
+
+    if (val > 5) {
+        result = 'Bigger than 5';
+    } else {
+        result = '5 or Smaller';
+    }
+
+    return result;
+}
+
+testElse(4);
+
+function testElseIf(val) {
+    if (val > 10) {
+        return 'Greater than 10';
+    } else if (val < 5) {
+        return 'Smaller than 5';
+    } else {
+        return 'Between 5 and 10';
+    }
+}
+
+testElseIf(7);
+
+function orderMyLogic(val) {
+    if (val < 5) {
+        return 'Less than 5';
+    } else if (val < 10) {
+        return 'Less than 10';
+    } else {
+        return 'Greater than or equal to 10';
+    }
+}
+
+orderMyLogic(7);
+
+function testSize(num) {
+    if (num < 5) {
+        return 'Tiny';
+    } else if (num < 10) {
+        return 'Small';
+    } else if (num < 15) {
+        return 'Medium';
+    } else if (num < 20) {
+        return 'Large';
+    } else if (num >= 20) {
+        return 'Huge';
+    } else {
+        return 'Change Me';
+    }
+}
+
+testSize(7);
+
+const names = [
+    'Hole-in-one!',
+    'Eagle',
+    'Birdie',
+    'Par',
+    'Bogey',
+    'Double Bogey',
+    'Go Home!',
+];
+
+function golfScore(par, strokes) {
+    if (strokes === 1) {
+        return 'Hole-in-one!';
+    } else if (strokes <= par - 2) {
+        return 'Eagle';
+    } else if (strokes === par - 1) {
+        return 'Birdie';
+    } else if (strokes === par) {
+        return 'Par';
+    } else if (strokes === par + 1) {
+        return 'Bogey';
+    } else if (strokes === par + 2) {
+        return 'Double Bogey';
+    } else if (strokes >= par + 3) {
+        return 'Go Home!';
+    } else {
+        return 'Change Me';
+    }
+}
+
+golfScore(5, 4);
