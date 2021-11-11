77edaefcd93fb94fde325a9e5d65dad7e8e7d091
Mikhail Shestak <stampout386@gmail.com>
2021-11-11 20:29:27 +0300

    96-100

--- Regular-expressions.js
+++ Regular-expressions.js
@@ -1,11 +1,11 @@
-let petString = 'James has a pet cat.';
-let petRegex = /dog|cat|bird|fish/; // Change this line
-let result = petRegex.test(petString);
+// let petString = 'James has a pet cat.';
+// let petRegex = /dog|cat|bird|fish/; // Change this line
+// let result = petRegex.test(petString);
 
-let myString = 'freeCodeCamp';
-let fccRegex = /freecodecamp/i; // Change this line
-let result = fccRegex.test(myString);
+// let myString = 'freeCodeCamp';
+// let fccRegex = /freecodecamp/i; // Change this line
+// let result = fccRegex.test(myString);
 
-let extractStr = "Extract the word 'coding' from this string.";
-let codingRegex = /coding/; // Change this line
-let result = extractStr.match(codingRegex); // Change this line
+// let extractStr = "Extract the word 'coding' from this string.";
+// let codingRegex = /coding/; // Change this line
+// let result = extractStr.match(codingRegex); // Change this line
--- basicJs.js
+++ basicJs.js
@@ -49,11 +49,9 @@ const quotient = 66 / 33;
 
 let myVar = 87;
 
-// Only change code below this line
 myVar++;
 let myVar = 11;
 
-// Only change code below this line
 myVar--;
 
 const ourDecimal = 5.7;
@@ -785,4 +783,31 @@ function pushArr() {
         myArray.push(i);
         i--;
     }
+}
+
+function pushArr2() {
+    const myArray = [];
+    for (let i = 1; i < 6; i++) {
+        myArray.push(i);
+    }
+    console.log(myArray);
+}
+
+pushArr2();
+
+const myArray = [];
+for (let i = 1; i < 10; i += 2) {
+    myArray.push(i);
+    console.log(myArray);
+}
+
+const myArray = [];
+for (let i = 9; i > 0; i -= 2) {
+    myArray.push(i);
+}
+
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+    total += myArr[i];
 }
