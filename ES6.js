92c1ea7403c0ddc5be931b881254278d0047e61f
Mikhail Shestak <stampout386@gmail.com>
2021-11-12 15:25:34 +0300

    06 ES6

--- ES6.js
+++ ES6.js
@@ -36,3 +36,7 @@ const myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1, 2], [3, 4, 5]));
 
 const increment = (number, value = 1) => number + value;
+
+const sum = (...args) => {
+    return args.reduce((a, b) => a + b, 0);
+};
