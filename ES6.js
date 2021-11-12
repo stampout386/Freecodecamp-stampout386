de065dd6b44924a6ff50e36b4038f54dd93adaf1
Mikhail Shestak <stampout386@gmail.com>
2021-11-12 17:33:03 +0300

    19 ES6

--- ES6.js
+++ ES6.js
@@ -143,3 +143,11 @@ const thermos = new Thermostat(76); // Setting in Fahrenheit scale
 let temp = thermos.temperature; // 24.44 in Celsius
 thermos.temperature = 26;
 temp = thermos.temperature; // 26 in Celsius
+
+{
+    /* <html>
+  <body>
+<script type ='module' src='index.js'></script>
+  </body>
+</html> */
+}
