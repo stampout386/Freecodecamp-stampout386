d01b66dd2a2bd90b7dfd73b8e3b00b64215ff614
Mikhail Shestak <stampout386@gmail.com>
2021-11-12 17:20:07 +0300

    16 ES6

--- ES6.js
+++ ES6.js
@@ -108,3 +108,12 @@ function makeList(arr) {
 const failuresList = makeList(result.failure);
 
 const createPerson = (name, age, gender) => ({name, age, gender});
+
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+        this.gear = newGear;
+    },
+};
+bicycle.setGear(3);
+console.log(bicycle.gear);
