8865f0ba52a4dddcea2e633363d6cd8e0c0c3626
Mikhail Shestak <stampout386@gmail.com>
2021-11-12 14:54:58 +0300

    02 ES6

--- ES6.js
+++ ES6.js
@@ -7,3 +7,10 @@ function checkScope() {
     console.log('Function scope i is: ', i);
     return i;
 }
+const s = [5, 7, 2];
+function editInPlace() {
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+}
+editInPlace();
