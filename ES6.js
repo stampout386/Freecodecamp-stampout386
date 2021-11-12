4488e4a46d874f4549414fc7a83e7f4b8d5de71e
Mikhail Shestak <stampout386@gmail.com>
2021-11-12 17:06:57 +0300

    12 ES6

--- ES6.js
+++ ES6.js
@@ -74,3 +74,10 @@ const {
 let a = 8,
     b = 6;
 [a, b] = [b, a];
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list) {
+    const [a, b, ...arr] = list;
+    return arr;
+}
+const arr = removeFirstTwo(source);
