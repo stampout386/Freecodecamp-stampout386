89741d6c8582cc5681ffacb1b0d843c1cf576ba3
Mikhail Shestak <stampout386@gmail.com>
2021-11-12 15:23:25 +0300

    05 ES6

--- ES6.js
+++ ES6.js
@@ -34,3 +34,5 @@ const magic = () => new Date();
 const myConcat = (arr1, arr2) => arr1.concat(arr2);
 
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+const increment = (number, value = 1) => number + value;
