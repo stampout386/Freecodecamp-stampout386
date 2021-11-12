29a4fa96e3eab0b32d8ae3220d5269f44fb7ff25
Mikhail Shestak <stampout386@gmail.com>
2021-11-12 20:27:10 +0300

    basicData 3

--- Basic-Data-Structures.js
+++ Basic-Data-Structures.js
@@ -3,3 +3,11 @@ let yourArray = [1, 'sdss', true, false, 45, 'hz'];
 let myArray = ['a', 'b', 'c', 'd'];
 myArray[1] = 23;
 console.log(myArray);
+
+function mixedNumbers(arr) {
+    arr.unshift('I', 2, 'three');
+    arr.push(7, 'VIII', 9);
+    return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
